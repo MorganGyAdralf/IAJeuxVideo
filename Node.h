32cbb2f495ac01a577a5f60db87e797361e4211b
MomoPython <morgan.gy55@gmail.com>
2020-04-22 16:49:54 +0200

    ajout

---
+++ Node.h
@@ -0,0 +1,16 @@
+#pragma once
+class Node {
+public:
+    Node();
+    Node(const Node& orig);
+    virtual ~Node();
+    bool hasNext();
+    Node* getNext();
+    void setNext(Node* newNext);
+    int getValue();
+    void setValue(int val);
+private:
+    Node* next;
+    int value;
+};
+
